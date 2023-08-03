be4ef786767c3c1b0f37e1887b58efd501301fa7
frako23 <frako23@gmail.com>
2023-08-03 15:38:36 -0400

    ajuste de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className={toggle ? "collapse" : "navbar-collapse"} id="navbarSupportedContent">
+          <div className={toggle ? "navbar-collapse" : "collapse"} id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
                 <a className="nav-link" href="index.html">
